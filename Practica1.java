3e118124edc024985ffe7fe3a9e72b892f7e9176
Nicolás Rotariu Peña <al448019@uji.es>
2025-10-08 10:58:18 +0200

    V.primera parte

--- Practica1.java
+++ Practica1.java
@@ -6,13 +6,56 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        HashSet<Integer> result= new HashSet<Integer>();
+        ArrayList<Integer> vistos= new ArrayList<Integer>();
+
+        while(it.hasNext()){
+            boolean added=false;
+            Integer e=it.next();
+
+            for(int i=0; i<vistos.size(); i++){
+                int n=vistos.get(i);
+                    if(e%n==0){
+                        result.add(e);
+                        added=true;
+
+                    } else if (n%e==0) {
+                        result.add(n);
+                        added=true;
+                    }
+            }
+
+            if(!added){
+            vistos.add(e);
+            }
+
+        }
+        return result;
     }
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+        Iterator<Integer> it= cuadrados.iterator();
+        Iterator<Integer> it2= noCuadrados.iterator();
+
+
+        while(it.hasNext()) {
+            Integer e1 = it.next();
+            Integer e1_cuadrado = e1 * e1;
+            if (cuadrados.contains(e1_cuadrado) || noCuadrados.contains(e1_cuadrado)) {
+                cuadrados.add(e1_cuadrado);
+                noCuadrados.add(e1);
+                cuadrados.remove(e1);
+                noCuadrados.remove(e1_cuadrado);
+            }
+
+            Integer e2 = it2.next();
+            Integer e2_cuadrado=e2*e2;
+            if (cuadrados.contains(e2_cuadrado) || noCuadrados.contains(e2_cuadrado)) {
+                cuadrados.add(e2_cuadrado);
+                noCuadrados.remove(e2_cuadrado);
+            }
+        }
 
     }
 
